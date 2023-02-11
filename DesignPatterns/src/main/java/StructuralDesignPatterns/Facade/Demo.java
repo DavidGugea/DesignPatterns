76c431a7e0b5189c7cf0c11a9a5274cda08bc3b4
David Gugea <davidgugea@yahoo.com>
2023-02-11 14:16:52 +0100

    Facade

---
+++ DesignPatterns/src/main/java/StructuralDesignPatterns/Facade/Demo.java
@@ -0,0 +1,12 @@
+package StructuralDesignPatterns.Facade;
+
+import StructuralDesignPatterns.Facade.facade.VideoConversionFacade;
+
+import java.io.File;
+
+public class Demo {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.gg", "mp4");
+    }
+}
---
+++ DesignPatterns/src/main/java/StructuralDesignPatterns/Facade/some_complex_media_library/Codec.java
@@ -0,0 +1,4 @@
+package StructuralDesignPatterns.Facade.some_complex_media_library;
+
+public interface Codec {
+}
