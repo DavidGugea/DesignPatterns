3a8cfe9487afa1914d5ca96053e0729672871499
David Gugea <davidgugea@yahoo.com>
2023-02-11 12:06:30 +0100

    Prototype

---
+++ DesignPatterns/src/main/java/CreationalDesignPatterns/Prototype/shapes/Circle.java
@@ -0,0 +1,26 @@
+package CreationalDesignPatterns.Prototype.shapes;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle() {}
+
+    public Circle(Circle target) {
+        super(target);
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if(!(object2 instanceof Circle)) return false;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
+}
