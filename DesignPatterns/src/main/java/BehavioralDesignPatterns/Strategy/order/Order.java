1c0afa840d5766c2fef0b367623d16f11fdfca4e
David Gugea <davidgugea@yahoo.com>
2023-02-11 20:10:09 +0100

    Strategy

---
+++ DesignPatterns/src/main/java/BehavioralDesignPatterns/Strategy/order/Order.java
@@ -0,0 +1,28 @@
+package BehavioralDesignPatterns.Strategy.order;
+
+import BehavioralDesignPatterns.Strategy.strategies.PayStrategy;
+
+public class Order {
+    private int totalCost = 0;
+    private boolean isClosed = false;
+
+    public void processOrder(PayStrategy strategy) {
+        strategy.collectPaymentDetails();
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+}
